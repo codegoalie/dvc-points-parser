bf68521351149d1a8fbe5ad510afba010d938ecf
Chris Marshall <chris@codegoalie.com>
2020-04-19 14:26:00 -0400

    Properly order room types for point amt parsing

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -75,7 +74,7 @@ func main() {
 func parseFile(file *os.File) (Resort, error) {
 	resort := Resort{}
 	state := 0
-	var err error
+	roomTypes := []RoomType{}
 	viewLegend := map[string]string{}
 	typeIndex := 0
 
@@ -91,15 +90,11 @@ func parseFile(file *os.File) (Resort, error) {
 		case 0:
 			parseName(&resort, line)
 		case 1:
-			parseRoomType(&resort, line)
+			parseRoomType(&roomTypes, line)
 		case 2:
 			parseViewLegend(&viewLegend, line)
 		case 3:
-			err = parseRoomViews(&resort, typeIndex, viewLegend, line)
-			if err != nil {
-				err = fmt.Errorf("failed to parse room views: %w", err)
-				return resort, err
-			}
+			parseRoomViews(&resort, roomTypes[typeIndex], viewLegend, line)
 			typeIndex++
 		default:
 			break
@@ -118,8 +113,8 @@ func parseName(resort *Resort, line string) {
 	resort.Name = line
 }
 
-func parseRoomType(resort *Resort, line string) {
-	resort.RoomTypes = append(resort.RoomTypes, RoomType{
+func parseRoomType(roomTypes *[]RoomType, line string) {
+	*roomTypes = append(*roomTypes, RoomType{
 		Name: line,
 	})
 }
@@ -129,28 +124,9 @@ func parseViewLegend(legend *map[string]string, line string) {
 	(*legend)[fields[0]] = fields[2]
 }
 
-func parseRoomViews(resort *Resort, typeIndex int, viewLegend map[string]string, line string) error {
-	if len(resort.RoomTypes) < 1 {
-		return errors.New("cannot add room views without any room types")
-	}
-
-	currentRoomType := &resort.RoomTypes[typeIndex]
-
+func parseRoomViews(resort *Resort, roomType RoomType, viewLegend map[string]string, line string) {
 	for _, viewKey := range strings.Fields(line) {
-		viewType := viewLegend[viewKey]
-		fmt.Println(viewKey, viewLegend["L"])
-
-		if currentRoomType.ViewType == "" {
-			currentRoomType.ViewType = viewType
-			continue
-		}
-
-		resort.RoomTypes = append(resort.RoomTypes, RoomType{
-			Name:        currentRoomType.Name,
-			Description: currentRoomType.Description,
-			ViewType:    viewType,
-		})
+		roomType.ViewType = viewLegend[viewKey]
+		resort.RoomTypes = append(resort.RoomTypes, roomType)
 	}
-
-	return nil
 }
